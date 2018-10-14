eb1f4a1d0be6ed2b1ce7badfe3d23fb54d2e511a
Robert <robert.abela.15@um.edu.mt>
2018-10-14 07:09:17 +0200

    added bmi calculator

---
+++ src/lesson03/if_conditions.py
@@ -0,0 +1,19 @@
+#BMI Calculator
+height = float(input("Enter height in metres:    "))
+weight = float(input("Enter weight in kilograms: "))
+
+bmi = weight / (height*height)
+label = ''
+
+if (bmi < 18.5):
+    label = 'underweight'
+elif bmi < 25:
+    label = 'normal'
+elif bmi < 30:
+    label = 'overweight'
+elif bmi < 35:
+    label = 'obese'
+else:
+    label = 'extremely obese'
+
+print("Your BMI is %.1f (%s)" % (bmi, label))
