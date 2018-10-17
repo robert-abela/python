d49e8799fb83b58e11a7892df7651179df415924
Robert <robert.abela.15@um.edu.mt>
2018-10-17 19:06:17 +0200

    commit super calc

---
+++ src/lesson04/super_calc.py
@@ -0,0 +1,9 @@
+def superCalc(n1, n2):
+    print(n1 + n2)
+    print(n1 - n2)
+    print(n1 * n2)
+    print(n1 / n2)
+
+n1 = int(input("enter n1: "))
+n2 = int(input("enter n2: "))
+superCalc(n1, n2)
