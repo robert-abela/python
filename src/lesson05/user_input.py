64ed53e63f3ffc87ace275efb583c2aced90074f
Robert (laptop) <robert.abela.15@um.edu.mt>
2018-10-23 23:04:14 +0200

    user input sample

---
+++ src/lesson05/user_input.py
@@ -0,0 +1,10 @@
+def input_number():
+    while True:
+        try:
+        	string = input("Enter a number: ")
+            num = int(string)
+            return num
+        except ValueError:
+            print("Not a valid numner, try again!")
+
+number = input_number()
