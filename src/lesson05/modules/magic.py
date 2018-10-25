3dacac315ce93e8bc287a575f7fab0f68da8a2c5
Robert Abela <robert-abela@users.noreply.github.com>
2018-10-25 09:51:22 +0200

    Update magic.py

--- src/lesson05/modules/magic.py
+++ src/lesson05/modules/magic.py
@@ -3,3 +3,25 @@ def trick_one():
   
 def trick_two():
   print('Sim Sala Bim') 
+
+def harry_potter(spell):
+  if spell == 10:
+    print('Riddikulus.')
+  elif spell == 9:
+    print('Obliviate.')
+  elif spell == 8:
+    print('Sectumsempra.')
+  elif spell == 7:
+    print('Avada Kedavra.')
+  elif spell == 6:
+    print('Alohomora.')
+  elif spell == 5:
+    print('Lumos.')
+  elif spell == 4:
+    print('Expelliarmus.')
+  elif spell == 3:
+    print('Wingardium Leviosa.')
+  elif spell == 2:
+    print('Accio.')
+  elif spell == 1:
+    print('Expecto Patronum.')
