bdb005bbdf3a652bbf3c3b50484659d0844fe6e7
Robert Abela <robert-abela@users.noreply.github.com>
2018-10-25 09:28:24 +0200

    Create main.py

---
+++ src/lesson05/modules/main.py
@@ -0,0 +1,17 @@
+# import the magic module
+import magic
+
+def do_some_magic():
+    magic.trick_one()
+    magic.trick_two()
+
+def main():
+    ans = input('Do you want to see some magic? ')
+    if ans == 'yes':
+      do_some_magic()
+    else:
+      print('Ok, Bye!')
+
+# If this script is executed, then main() will be executed
+if __name__ == '__main__':
+    main()
