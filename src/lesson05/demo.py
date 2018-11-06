9e34800c6a5e6322234235258f32f4f5d16f7316
Robert Abela <robert-abela@users.noreply.github.com>
2018-11-06 04:36:11 +0100

    Update demo.py

--- src/lesson05/demo.py
+++ src/lesson05/demo.py
@@ -1,8 +1,8 @@
 def input_idcard(msg):
     while True:
         idcard = input(msg)
-        lastchar = idcard[-1]
-        num = idcard[0:-1]
+        lastchar = idcard[-1]   #only last character
+        num = idcard[0:-1]      #all characters apart from last
         
         if lastchar not in 'ABGHLMPZ':
             print('wrong ending')
@@ -18,9 +18,9 @@ def input_idcard(msg):
 def input_number(msg):
     '''Reads from user until a valid number.
 
-       Takes a string as an argument to show to the user. will
-       repeat user input until a valid integer is entered. Finally
-       it will return the int'''
+       Takes a string as an argument to show to the user. It will
+       repeat user input until a valid integer is entered. 
+       Finally it will return the int'''
     while True:
         try:
             return int(input(msg))
@@ -29,10 +29,9 @@ def input_number(msg):
 
 print(input_number.__doc__)
 
-ret_value = input_number('Enter your salary:')
+ret_value = input_number('Enter your salary: ')
 print(ret_value)
-ret_value = input_number('Enter your age:')
+ret_value = input_number('Enter your age: ')
 print(ret_value)
 ret_value = input_idcard('Enter IDCard number: ')
 print(ret_value)
-
