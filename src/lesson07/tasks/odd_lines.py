d86dd93e69ca1dd384df2e1dafa8463b250b946c
Robert (laptop) <robert.abela.15@um.edu.mt>
2020-08-31 21:45:58 +0200

    lesson updates

--- src/lesson07/tasks/odd_lines.py
+++ src/lesson07/tasks/odd_lines.py
@@ -6,3 +6,17 @@ for line in f:
   if counter % 2 == 1:
     print(line)
 f.close()
+
+## Done during lesson 31.08.2020
+
+try:
+	to_open = 'lorem.txt'
+	f = open(to_open)
+	print_line = True
+	for line in f:
+		if print_line == True:
+			print(line)
+		print_line = not print_line
+	f.close()
+except IOError:
+	print("Error")
