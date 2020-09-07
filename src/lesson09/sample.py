617b91a4890ccb64764a07b3e2c1283b3b072c46
Robert (laptop) <robert.abela.15@um.edu.mt>
2020-09-07 21:35:09 +0200

    samples from lesson 9

---
+++ src/lesson09/sample.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+try:
+	page = requests.get("https://www.python.org/search/?q=strings")
+except:
+	print("Failed to get HTML from the Internet")
+else:
+	soup = BeautifulSoup(page.content, 'html.parser')
+	ul = soup.find('ul', class_="list-recent-events") 
+	links = ul.find_all('a')
+	for link in links:
+		print(link.text)
